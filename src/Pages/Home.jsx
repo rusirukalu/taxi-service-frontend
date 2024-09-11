b8c13951c5d929053e11a60fbf06ad18616e6c82
Kavindu <kavinduindumina1@gmail.com>
2024-09-11 21:53:08 +0530

    Folder Structure Created

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/Navbar'
+
+// this is the home page
+// click nav bar home tab and load this page
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+      <h1>Home page</h1>
+    </div>
+  )
+}
