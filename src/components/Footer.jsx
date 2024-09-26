86810ffaa2791eb5bf1a653525ee8ed10df9dbbb
Kavindu <kavinduindumina1@gmail.com>
2024-09-26 18:35:39 +0530

    footer modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,83 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button'; // Add this line
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Add this line
-import { faFacebookF, faTwitter, faGoogle, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'; // Add this line
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaFacebookF, FaTwitter, FaGoogle, FaInstagram, FaLinkedin, FaGithub, FaGem, FaHome, FaEnvelope, FaPhone, FaPrint } from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <div>
-      <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-warning mt-5">
-          <div className="text-white mb-3 mb-md-0">Copyright © 2024. All rights reserved.</div>
-          <div>
-            <Button variant="link" className="text-white mx-2">
-              <FontAwesomeIcon icon={faFacebookF} />
-            </Button>
-            <Button variant="link" className="text-white mx-2">
-              <FontAwesomeIcon icon={faTwitter} />
-            </Button>
-            <Button variant="link" className="text-white mx-2">
-              <FontAwesomeIcon icon={faGoogle} />
-            </Button>
-            <Button variant="link" className="text-white mx-2">
-              <FontAwesomeIcon icon={faLinkedinIn} />
-            </Button>
-          </div>
+    <footer className="bg-light text-center text-lg-start text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+          <a href="#" className="me-4 text-reset">
+            <FaFacebookF />
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <FaTwitter />
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <FaGoogle />
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <FaInstagram />
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <FaLinkedin />
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <FaGithub />
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <Container className="text-center text-md-start mt-5">
+          <Row className="mt-3">
+            <Col md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <FaGem className="me-3" />
+                City Taxi
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </Col>
+
+            <Col md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <p><a href="#!" className="text-reset">Angular</a></p>
+              <p><a href="#!" className="text-reset">React</a></p>
+              <p><a href="#!" className="text-reset">Vue</a></p>
+              <p><a href="#!" className="text-reset">Laravel</a></p>
+            </Col>
+
+            <Col md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p><a href="#!" className="text-reset">Pricing</a></p>
+              <p><a href="#!" className="text-reset">Settings</a></p>
+              <p><a href="#!" className="text-reset">Orders</a></p>
+              <p><a href="#!" className="text-reset">Help</a></p>
+            </Col>
+
+            <Col md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <p><FaHome className="me-2" /> Galle, Southern Province, Sri Lanka</p>
+              <p><FaEnvelope className="me-3" /> info@citytaxi.com</p>
+              <p><FaPhone className="me-3" /> + 01 234 567 88</p>
+              <p><FaPrint className="me-3" /> + 01 234 567 89</p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2024 Copyright: 
+        <a className="text-reset fw-bold" href="#">
+           City-Taxi
+        </a>
       </div>
-    </div>
-  )
+    </footer>
+  );
 }
