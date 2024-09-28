7dae381ce36022f90ec6d69e1b4dc38053ddd52d
Kavindu <kavinduindumina1@gmail.com>
2024-09-28 19:45:43 +0530

    remove Chakra and update

--- src/index.js
+++ src/index.js
@@ -4,26 +4,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'; 
-import theme from './theme';
+
 
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot is used in React 18
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
-
-
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
 
--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,14 @@
 
 
-// Ensure breakpoints are included in the theme
-const theme = ç({
-  breakpoints: {
-    sm: "30em",
-    md: "48em",
-    lg: "62em",
-    xl: "80em",
-  },
-  // Add any other customizations to your theme here
-});
+// // Ensure breakpoints are included in the theme
+// const theme = ç({
+//   breakpoints: {
+//     sm: "30em",
+//     md: "48em",
+//     lg: "62em",
+//     xl: "80em",
+//   },
+//   // Add any other customizations to your theme here
+// });
 
-export default theme;
+// export default theme;
