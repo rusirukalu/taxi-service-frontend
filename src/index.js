a82f7464c727cb9794810b3a8c4c7db7cfba72f4
Nethmi Wickramasinghe <132387157+nethmi2000@users.noreply.github.com>
2024-09-29 13:54:12 +0530

    deleted theme.js and removed chackraUI

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'; 
-import theme from './theme';
 
 
 const container = document.getElementById('root');
